b11e2f33b01d6ac1fac6ebea9becc6b38582540f
HyeonjeongHeo0422 <113971055+HyeonjeongHeo0422@users.noreply.github.com>
2024-11-21 11:02:29 +0900

    uam_new_waypoint_cluster_analysis_new.csv 파일 통계

--- uam_route_clustering_analysis_visualize.py
+++ uam_route_clustering_analysis_visualize.py
@@ -10,7 +10,8 @@ def match_zero_position(ax1, ax2):
     return ax2_start
     
 # Load clustering results CSV
-file_path = 'results/clustering/uam_waypoint_cluster_analysis_new.csv'
+# file_path = 'results/clustering/uam_waypoint_cluster_analysis_new.csv'
+file_path = 'results/clustering/uam_new_waypoint_cluster_analysis_new.csv'
 results_df = pd.read_csv(file_path)
 
 # Add Waypoint Index as a column if it's not present
@@ -21,18 +22,18 @@ if 'Waypoint_Index' not in results_df.columns:
 results_df['Optimal_Cluster_Area_km2'] = results_df['Optimal_Cluster_Area'] / 1_000_000
 
 # Calculate and print statistics
-cluster_count_stats = results_df['Cluster_Count'].agg(['mean', 'min', 'max'])
-optimal_area_stats = results_df['Optimal_Cluster_Area_km2'].agg(['mean', 'min', 'max'])
 distance_stats = results_df['Distance_to_UAM'].agg(['mean', 'min', 'max'])
+optimal_area_stats = results_df['Optimal_Cluster_Area_km2'].agg(['mean', 'min', 'max'])
+cluster_polygon_count_stats = results_df['Optimal_Cluster_Polygon_Count'].agg(['mean', 'min', 'max'])
 
-print("\nCluster Count Statistics")
-print(cluster_count_stats)
+print("\nMin Distance to UAM Statistics (km)")
+print(distance_stats)
 
 print("\nSelected Cluster Area Statistics (km²)")
 print(optimal_area_stats)
 
-print("\nMin Distance to UAM Statistics (km)")
-print(distance_stats)
+print("\nCluster Polygon Count Statistics")
+print(cluster_polygon_count_stats)
 
 # 시각화: 클러스터 개수, 대표 클러스터 면적, 대표 클러스터와의 거리
 fig, ax1 = plt.subplots(figsize=(12, 6))
@@ -40,24 +41,25 @@ fig, ax1 = plt.subplots(figsize=(12, 6))
 # Left y-axis (Cluster Count, Max Area)
 ax1.plot(
     results_df['Waypoint_Index'],
-    results_df['Cluster_Count'],
+    results_df['Optimal_Cluster_Polygon_Count'],
     linestyle='-',
     linewidth=2,
     color='orange',
-    label='Cluster Count'
+    label='Number of Candidates'
 )
 
-ax1.plot(
-    results_df['Waypoint_Index'],
-    results_df['Optimal_Cluster_Area_km2'],
-    linestyle='-',
-    linewidth=2,
-    color='blue',
-    label='Selected Cluster Area (km²)'
-)
+# ax1.plot(
+#     results_df['Waypoint_Index'],
+#     results_df['Optimal_Cluster_Area_km2'],
+#     linestyle='-',
+#     linewidth=2,
+#     color='blue',
+#     label='Selected Cluster Area (km²)'
+# )
 
+ax1.set_ylim(bottom=0)
 ax1.set_xlabel('Waypoint Index', fontsize=20)
-ax1.set_ylabel('Cluster Count / Selected Cluster Area (km²)', fontsize=20)
+ax1.set_ylabel('Number of Candidates', fontsize=20)
 ax1.tick_params(axis='y', labelsize=20)
 ax1.tick_params(axis='x', labelsize=20)
 ax1.grid(alpha=0.5)
@@ -70,9 +72,19 @@ ax2.plot(
     linestyle='-',
     linewidth=2,
     color='green',
-    label='Min Distance (km)'
+    label='Distance (km)'
 )
-ax2.set_ylabel('Min Distance (km)', fontsize=20)
+
+ax2.plot(
+    results_df['Waypoint_Index'],
+    results_df['Optimal_Cluster_Area_km2'],
+    linestyle='-',
+    linewidth=2,
+    color='blue',
+    label='Cluster Area (km²)'
+)
+
+ax2.set_ylabel('Distance (km) / Cluster Area (km²)', fontsize=20)
 ax2.tick_params(axis='y', labelsize=20)
 
 # Align zero positions and ensure max value is 4.0
@@ -90,12 +102,13 @@ adjustment_ratio = desired_ax2_max / aligned_ax2_max
 adjusted_ax2_min = aligned_ax2_min * adjustment_ratio
 
 # Set the new range for ax2
-ax2.set_ylim(adjusted_ax2_min, desired_ax2_max)
+# ax2.set_ylim(adjusted_ax2_min, desired_ax2_max)
+ax2.set_ylim(0,4)
 
 # Add legends
 lines_1, labels_1 = ax1.get_legend_handles_labels()
 lines_2, labels_2 = ax2.get_legend_handles_labels()
-ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper right', fontsize=20)
+ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper center', fontsize=20)
 
 # Final adjustments
 plt.xlim(0, len(results_df) - 1)  # x축 범위를 0에서 데이터 개수로 설정
